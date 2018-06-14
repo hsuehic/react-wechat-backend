b50ccd337066a00136e89e00f3323194ec98ae3b
兴斋 <xingzhai.xxw@alibaba-inc.com>
2018-06-14 17:27:10 +0800

    chore: lint.

--- app.js
+++ app.js
@@ -1,57 +1,58 @@
-const Koa = require('koa')
-const views = require('koa-views')
-const json = require('koa-json')
-const onerror = require('koa-onerror')
-const bodyparser = require('koa-bodyparser')
-const logger = require('koa-logger')
-const session = require('koa-generic-session')
-const redis = require('koa-redis')
+const path = require('path');
+const Koa = require('koa');
+const views = require('koa-views');
+const json = require('koa-json');
+const onerror = require('koa-onerror');
+const bodyparser = require('koa-bodyparser');
+const logger = require('koa-logger');
+const session = require('koa-generic-session');
+const redis = require('koa-redis');
+const koaStatic = require('koa-static');
 
-var redisStore = require('koa-redis')({
+let redisStore = redis({
   // Options specified here
 });
 
-
-const app = new Koa()
+const app = new Koa();
 
 app.keys = ['keys', 'keykeys'];
 app.use(session({
   store: redisStore
 }));
 
-const index = require('./routes/index')
-const users = require('./routes/users')
+const index = require('./routes/index');
+const users = require('./routes/users');
 
 // error handler
-onerror(app)
+onerror(app);
 
 // middlewares
 app.use(bodyparser({
-  enableTypes:['json', 'form', 'text']
-}))
-app.use(json())
-app.use(logger())
-app.use(require('koa-static')(__dirname + '/public'))
+  enableTypes: ['json', 'form', 'text']
+}));
+app.use(json());
+app.use(logger());
+app.use(koaStatic(path.resolve('./public')));
 
-app.use(views(__dirname + '/views', {
+app.use(views(path.resolve('./views'), {
   extension: 'pug'
-}))
+}));
 
 // logger
-app.use(async (ctx, next) => {
-  const start = new Date()
-  await next()
-  const ms = new Date() - start
-  console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
-})
+app.use(async(ctx, next) => {
+  const start = new Date();
+  await next();
+  const ms = new Date() - start;
+  console.log(`${ctx.method} ${ctx.url} - ${ms}ms`);
+});
 
 // routes
-app.use(index.routes(), index.allowedMethods())
-app.use(users.routes(), users.allowedMethods())
+app.use(index.routes(), index.allowedMethods());
+app.use(users.routes(), users.allowedMethods());
 
 // error-handling
 app.on('error', (err, ctx) => {
-  console.error('server error', err, ctx)
+  console.error('server error', err, ctx);
 });
 
-module.exports = app
+module.exports = app;
