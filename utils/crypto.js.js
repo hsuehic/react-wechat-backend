f578922b7ce26d26635e4b4acd3fee4d01422f10
兴斋 <xingzhai.xxw@alibaba-inc.com>
2018-06-16 14:49:39 +0800

    feat: encrypt help methods.

---
+++ utils/crypto.js.js
@@ -0,0 +1,21 @@
+/**
+ * @File   : crypto.js.js
+ * @Author : Richard (xiaowei.hsueh@gmail.com)
+ * @Link   : http://www.gistop.com/
+ * @Date   : 2018-6-16 14:32:41
+ */
+
+const crypto = require('crypto');
+
+const md5 = crypto.createHash('md5');
+
+/**
+ * 使用md5方法加密码
+ * @param {string} input 需要加密的字符串
+ * @return {string} 加密码生成的字符串
+ */
+const encryptUsingMd5 = input => md5.update(input).digest('hex');
+
+module.exports = {
+  encryptUsingMd5
+};
