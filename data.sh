ffcfd350f6ee7b8d82fc5e43667a42c4135831c5
兴斋 <xingzhai.xxw@alibaba-inc.com>
2018-07-05 00:05:24 +0800

    chore: database install.

--- data.sh
+++ data.sh
@@ -3,3 +3,7 @@
 # sudo mongodump -h 127.0.0.1:27017 -d wechat -o ./db
 
 mongodump -h 127.0.0.1:27017 -d wechat -o ./db
+scp -r ./db root@www.gismall.com:/root/apps/wechat
+ssh root@www.gismall.com << remotessh
+  mongorestore -h 127.0.0.1:27017 -d wechat --dir /root/apps/wechat/db/wechat/
+remotessh
